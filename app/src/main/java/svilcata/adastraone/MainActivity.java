198abd3fc54a33cf246eb317dcd6466493e19dc7
Svilcata <jibryto1@gmail.com>
2017-10-30 16:47:16 +0200

    small fix in MainActivity class

--- app/src/main/java/svilcata/adastraone/MainActivity.java
+++ app/src/main/java/svilcata/adastraone/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
         getSupportActionBar().setTitle("Dogs Gallery");
 
         dogsAdapter = new DogsAdapter(dogList);
